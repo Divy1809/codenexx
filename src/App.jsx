93aef459cb2acb5ec0a2dbcff17ac81a77f1f77f
Divy1809 <divyjain1805@gmail.com>
2024-10-16 16:28:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,423 @@
+import React from 'react'
+import logo from './assets/logo1342.png'
+import code_icon from './assets/Group50.png'
+import codenex from './assets/logo3[1]2.png'
+import instagram from './assets/group22.png'
+import x from './assets/group23.png'
+import linkedin from './assets/group24.png'
+import whatsapp from './assets/group25.png'
+import rectangle8 from './assets/rectangle8.png'
+import down from './assets/down.png'
+import rectangle50 from './assets/rectangle50.png'
+import group20 from './assets/group20.png'
+import icon076 from './assets/076.png'
+import icon077 from './assets/077.png'
+import E1 from './assets/1.png'
+import E2 from './assets/2.png'
+import E3 from './assets/3.png'
+import E4 from './assets/4.png'
+import E5 from './assets/5.png'
+import E6 from './assets/6.png'
+import E7 from './assets/7.png'
+import E8 from './assets/8.png'
+import nontech from './assets/nontech.png'
+import tech from './assets/tech.png'
+import creative from './assets/creative.png'
+
+
+function App() {
+  
+  return (
+    <div className='app  min-h-screen text-white relative'>
+
+      <div>
+        <div className="absolute top-[4.6vh] left-[5.2vh]" >
+          <img src={logo} alt="Logo" className="w-[63px] h-[63px]" />
+        </div>
+        <div className="absolute top-[3.38vh] left-[52%]" >
+  <nav className="flex justify-center items-center p-6 bg-transparent">
+    <ul className="flex gap-5 list-none nav">
+      <li>
+        <a href="App.jsx" className="link">HOME</a>
+      </li>
+      <li>
+        <a href="App.jsx" className="link">TEAM</a>
+      </li>
+      <li>
+        <a href="App.jsx" className="link">COURSE</a>
+      </li>
+      <li>
+        <a href="App.jsx" className="link">NEWSLETTER</a>
+      </li>
+      <li>
+        <a href="App.jsx" className="link">CONTACT</a>
+      </li>
+    </ul>
+  </nav>
+</div>
+
+      </div> 
+      
+
+
+
+      <div>
+        <div className='absolute top-[26.38vh] left-[67%]' >
+          <img src={code_icon} alt="" className='w-[59.93vh] h-[64.14vh]' />
+        </div>
+        <div className='absolute top-[42.62vh] left-[18.8vh]' >
+          <h1 className='w-[91.47vh] h-[19.35vh]' style={{
+            fontFamily: 'Inter',
+            fontSize: '14.88vh',
+            fontWeight: 700,
+            lineHeight: '18.01vh',
+            textAlign: 'left',
+            color: '#FFFFFF',
+          }}>Welcome to</h1>
+        </div>
+        <div className='absolute' style={{ top: '37.15vh', left: '10.82vh', rotate: '9.54deg' }}>
+          <img src={down} alt="" className='w-[17.82vh] h-[13.7vh]' />
+        </div>
+        <div className='absolute' style={{ top: '60.62vh', left: '17.99vh' }}>
+          <img src={codenex} alt="" className='w-[74.9vh] h-[18.13vh]' />
+        </div>
+        <div className='absolute' style={{ top: '61.02vh', left: '91.33vh' }}>
+          <h1 className='w-[87.41vh] h-[17.99vh]' style={{
+            fontFamily: 'Inter',
+            fontSize: '14.88vh',
+            fontWeight: 700,
+            lineHeight: '18.01vh',
+            textAlign: 'left',
+            color: '#FFFFFF',
+          }}>Club</h1>
+        </div>
+        <div className='absolute ' style={{ top: '83.08vh', left: '21.65vh' }}>
+          <h1 className='w-[87.41vh] h-[17.99vh]' style={{
+            fontFamily: 'Inter',
+            fontSize: '5.41vh',
+            fontWeight: 600,
+            lineHeight: '6.55vh',
+            textAlign: 'left',
+            color: '#FFFFFF',
+          }}>Were the tech club of srm</h1>
+        </div>
+        <div className='absolute flex space-x-4' style={{ top: '105vh', left: '81.86vh' }}>
+          <a href="https://www.instagram.com/codenex_srmist?igsh=aXo0ajFqN3dhYmMy">
+            <img src={instagram} alt="" className='w-[6.76vh],h-[6.76vh]' />
+          </a>
+          <a href="https://x.com/i/flow/login">
+            <img src={x} alt="" className='w-[6.76vh],h-[6.76vh]' />
+          </a>
+          <a href="https://www.linkedin.com/company/code-nex-club-srmist/">
+            <img src={linkedin} alt="" className='w-[6.76vh],h-[6.76vh]' />
+          </a>
+          <a href="https://web.whatsapp.com/">
+            <img src={whatsapp} alt="" className='w-[6.76vh],h-[6.76vh]' />
+          </a>
+        </div>
+      </div>
+
+      <div>
+        <div className='absolute about' style={{ top: '137.88vh', left: '9.47vh' }}>
+          <h1 className='w-[13.93vh] h-[3.24vh]' style={{
+            fontFamily: 'Inter',
+            fontSize: '2.7vh',
+            fontWeight: 400,
+            lineHeight: '3.27vh',
+            textAlign: 'left',
+            color: '#FFFFFF',
+          }}>ABOUT US</h1>
+        </div>
+        
+        <div className='absolute about' style={{ top: '145.87vh', left: '9.47vh' }}>
+          <p className='w-[82.13vh] h-[12.17vh]' style={{
+            fontFamily: 'Inter',
+            fontSize: '4.33vh',
+            fontWeight: 700,
+            lineHeight: '6.08vh',
+            textAlign: 'left',
+            color: '#FFFFFF',
+          }}>Lorem Ipsum is simply dummy text of <br />the printing and typesetting industry. </p>
+        </div>
+        <div className='absolute about' style={{ top: '164.81vh', left: '9.47vh' }}>
+          <p className='w-[82.13vh] h-[12.17vh]' style={{
+            fontFamily: 'Inter',
+            fontSize: '4.33vh',
+            fontWeight: 700,
+            lineHeight: '6.08vh',
+            textAlign: 'left',
+            color: '#FFFFFF',
+          }}>Lorem Ipsum has been the industry's standard dummy text ever since the <br />1500s, when an unknown printer took <br /> a galley of type and scrambled it to <br /> make a type specimen book. </p>
+        </div>
+        <div className='absolute' style={{ top: '143.43vh', left: '113.66vh' }}>
+          <img src={rectangle8} alt="" className='w-[71.71vh] h-[64.14vh]' />
+        </div>
+        <div
+          className='absolute'
+          style={{
+            top: '202.16vh',
+            left: '9.87vh',
+            width: '24.62vh',
+            height: '7.84vh',
+            padding: '2.43vh 3.51vh',
+            gap: '1.35vh',
+            borderRadius: '3.99vh',
+            border: '0.27vh solid #9747FF',
+          }}
+        >
+          <a href="https://www.srmist.edu.in/"><p
+            style={{
+              color: '#9747FF',
+              cursor: 'pointer',
+              fontSize: '2.16vh',
+              fontWeight: '54.12vh',
+              lineHeight: '3.04vh',
+              textAlign: 'centre',
+              width: '17.59vh',
+              height: '2.97vh',
+            }}
+          >MEET THE TEAM
+          </p></a>
+        </div>
+      </div>
+      <div className='absolute w-full flex justify-center' style={{
+        top:'236.26vh',
+      }}>
+        <marquee >
+          <div className='scroller flex space-x-32' style={{fontSize:'6.76vh',
+            fontFamily:'Inter',
+            fontWeight:350,
+            textAlign:'center',
+          }}>
+            <button>DESIGN</button>
+            <button>TEAMS</button>
+            <button>CREATIVE</button>
+            <button>START-UPS</button>
+            <button>INTERNSHIPS</button>
+            <button>UI/UX</button>
+            <button>MARKETING</button>
+            <button>HACKATHONS</button>
+            <button>APPLICATIONS</button>
+            </div>
+        </marquee>
+      </div>
+
+
+      <div className='absolute ' style={{ top: '274.99vh', left: '82.8vh', letterSpacing: '0.2em' }}>
+        <p className='w-[48.98vh] h-[6.08vh] ' style={{
+          fontFamily: 'Inter',
+          fontSize: '4.33vh',
+          fontWeight: 500,
+          lineHeight: '6.08vh',
+          textAlign: 'centre',
+          color: '#FFFFFF',
+          letterSpacing: '0.2em'
+        }}>OUR DOMAINS</p>
+      </div>
+      <div className="domains flex justify-center" style={{top:'284.16vh',left:'23.68vh',alignItems:'center',minHeight:'100vh',position:'relative',width:'155.61vh',padding:'2.7vh'}}>
+        <div className="card" style={{position:'relative',width:'40.59vh',height:'56.83vh',backgroundColor:'#fff', margin:'2.7vh',borderRadius:'2.7vh',overflow:'hidden',boxShadow:'0 2.02vh 3.38vh rgba(0, 0, 0, .2)',  transition: '.5s ease-in-out'}}>
+          <div className="circle">
+            <h2 style={{
+              fontSize:'5.68vh'
+            }}>Non-Technical</h2>
+          </div>
+          <div className="content"  style={{
+            fontSize:'4.33vh'
+          }}>
+            <p>this is no tecical domain</p>
+          </div>
+        </div>
+        <div className="card" style={{position:'relative',width:'40.59vh',height:'56.83vh',backgroundColor:'#fff', margin:'2.7vh',borderRadius:'2.7vh',overflow:'hidden',boxShadow:'0 2.02vh 3.38vh rgba(0, 0, 0, .2)',  transition: '.5s ease-in-out'}}>
+          <div className="circle">
+            <h2 style={{
+              fontSize:'5.68vh'
+            }}>Technical</h2>
+          </div>
+          <div className="content" style={{
+            fontSize:'4.33vh'
+          }}>
+           <p>this is technical domain</p>
+          </div>
+        </div>
+        <div className="card" style={{position:'relative',width:'40.59vh',height:'56.83vh',backgroundColor:'#fff', margin:'2.7vh',borderRadius:'2.7vh',overflow:'hidden',boxShadow:'0 2.02vh 3.38vh rgba(0, 0, 0, .2)',  transition: '.5s ease-in-out'}}>
+          <div className="circle">
+            <h2 style={{
+              fontSize:'5.68vh'
+            }}>Creatives</h2>
+          </div>
+          <div className="content" style={{
+            fontSize:'4.33vh'
+          }}>
+            <p>this is creative domain</p>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='absolute justify-center ' style={{ top: '400.1vh', left: '67.8vh', letterSpacing: '0.2em' }}>
+        <p className='w-[77.13vh] h-[6.08vh] ' style={{
+          fontFamily: 'Inter',
+          fontSize: '4.33vh',
+          fontWeight: 500,
+          lineHeight: '5.96vh',
+          textAlign: 'centre',
+          color: '#FFFFFF',
+          letterSpacing: '0.2em'
+        }}>Let's check out our socials</p>
+      </div>
+    <div className='absolute container  justify-center' style={{alignItems:'center',minHeight:'100vh',  top: '372.39vh', left: '84.57vh'
+}} >
+        <span  className='w-[20.29vh] h-[27.06vh] flex justify-center' style={{'--i':1,}}>
+          <img src={E1} alt="" />
+          </span>
+        <span  className='w-[20.29vh] h-[27.06vh] flex justify-center' style={{'--i':2}}>
+          <img src={E2} alt="" />
+          </span>
+        <span  className='w-[20.29vh] h-[27.06vh] flex justify-center' style={{'--i':3}}>
+          <img src={E3} alt="" />
+          </span>
+        <span  className='w-[20.29vh] h-[27.06vh] flex justify-center' style={{'--i':4}}>
+          <img src={E4} alt="" />
+          </span>
+        <span  className='w-[20.29vh] h-[27.06vh] flex justify-center' style={{'--i':5}}>
+          <img src={E5} alt="" />
+          </span>
+        <span className='w-[20.29vh] h-[27.06vh] flex justify-center'  style={{'--i':6}}>
+          <img src={E6} alt="" />
+          </span>
+        <span  className='w-[20.29vh] h-[27.06vh] flex justify-center' style={{'--i':7}}>
+          <img src={E7} alt="" />
+          </span>
+        <span  className='w-[20.29vh] h-[27.06vh] flex justify-center' style={{'--i':8}}>
+          <img src={E8} alt="" />
+          </span>
+
+      </div>
+     
+      <div className='absolute justify-center ' style={{ top: '550.87vh', left: '83.62vh' }}>
+        <p className='w-[27.6vh] h-[3.24vh] ' style={{
+          fontFamily: 'Inter',
+          fontSize: '2.7vh',
+          fontWeight: 400,
+          lineHeight: '3.27vh',
+          textAlign: 'centre',
+          color: '#939395'
+        }}>Want to Collaborate ?</p>
+      </div>
+
+      <div className='absolute justify-center' style={{ top: '555.34vh', left: '59.67vh' }}>
+        <p className='w-[75.50vh] h-[20.29vh] ' style={{
+          fontFamily: 'Inter',
+          fontSize: '16.77vh',
+          fontWeight: 600,
+          lineHeight: '20.30vh',
+          textAlign: 'centre',
+          color: '#FFFFFF',
+        }}>Let's Talk</p>
+      </div>
+      <div
+        className='absolute'
+        style={{
+          top: '577.67vh',
+          left: '84.97vh',
+          width: '24.89vh',
+          height: '7.84vh',
+          padding: '2.43vh 5.41vh',
+          gap: '1.35vh',
+          borderRadius: '3.99vh',
+          border: '0.27vh solid #9747FF',
+        }}>
+        <a href="mailto:codenex.club@srmist.com"><p
+          style={{
+            color: '#9747FF',
+            cursor: 'pointer',
+            fontSize: '2.16vh',
+            fontWeight: '400',
+            lineHeight: '3.04vh',
+            textAlign: 'centre',
+            width: '14.07vh',
+            height: '2.97vh',
+          }}>Contact us
+        </p></a>
+      </div>
+      <div className='absolute' style={{ top: '533.69vh', left: '131.66vh' }}>
+        <img src={group20} alt="" className='w-[53.08vh] h-[47.85vh]' />
+      </div>
+      <div className="absolute"
+        style={{
+          width: '194.85vh',
+          height: '0',
+          top: '598.51vh',
+         
+          border: '0.13vh solid #E8E7EA',
+          opacity: '1',
+        }}></div>
+      <footer>
+        <div className='absolute' style={{ top: '604.33vh', left: '0.62vh', rotate: '-10.41deg' }}>
+          <img src={icon076} alt="" className='w-[9.03vh] h-[4.95vh]' />
+        </div>
+        <div className='absolute' style={{ top: '608.41vh', left: '56.8vh', rotate: '7.93deg' }}>
+          <img src={icon077} alt="" className='w-[12.51vh] h-[5.63vh]' />
+        </div>
+        <div className='absolute justify-center ' style={{ top: '604.63vh', left: '10.97vh' }}>
+          <p className='w-[49.39vh] h-[7.57vh] ' style={{
+            fontFamily: 'Inter',
+            fontSize: '3.11vh',
+            fontWeight: 500,
+            lineHeight: '3.76vh',
+            textAlign: 'left',
+            color: '#FFFFFF',
+          }}>Codenex Club is one of the most <br />active clubs in whole SRM.</p>
+        </div>
+        <div className='absolute flex space-x-4' style={{ top: '623.27vh', left: '70px9.47vh' }}>
+          <a href="https://www.instagram.com/codenex_srmist?igsh=aXo0ajFqN3dhYmMy">
+            <img src={instagram} alt="" className='w-[50px6.76vh],h-[50px6.76vh]' />
+          </a>
+          <a href="https://x.com/i/flow/login">
+            <img src={x} alt="" className='w-[50px6.76vh],h-[50px6.76vh]' />
+          </a>
+          <a href="https://www.linkedin.com/company/code-nex-club-srmist/">
+            <img src={linkedin} alt="" className='w-[50px6.76vh],h-[50px6.76vh]' />
+          </a>
+          <a href="https://web.whatsapp.com/">
+            <img src={whatsapp} alt="" className='w-[50px6.76vh],h-[50px6.76vh]' />
+          </a>
+        </div>
+        <div className='absolute justify-center ' style={{ top: '614.91vh', left: '124.35vh' }}>
+          <a href="https://www.srmist.edu.in/"><p className='w-[61.02vh] h-[3.78vh] ' style={{
+            fontFamily: 'Inter',
+            fontSize: '3.11vh',
+            fontWeight: 500,
+            lineHeight: '3.76vh',
+            textAlign: 'right',
+            color: '#FFFFFF',
+          }}>SRMIST KTR</p></a>
+        </div>
+        <div className='absolute justify-center ' style={{ top: '615.94vh', left: '146.87vh' }}>
+          <a href="mailto:codenex.club@srmist.com"><p className='w-[39.51vh] h-[3.788vh] ' style={{
+            fontFamily: 'Inter',
+            fontSize: '3.11vh',
+            fontWeight: 500,
+            lineHeight: '3.76',
+            textAlign: 'left',
+            color: '#FFFFFF',
+          }}>codenex.club@srmist.com</p></a>
+        </div>
+        <div className='absolute justify-center ' style={{ top: '625.03vh', left: '158.32vh' }}>
+          <a href="tel:+91-0000000000">
+            <p className='w-[27.06vh] h-[3.788vh] ' style={{
+            fontFamily: 'Inter',
+            fontSize: '3.11vh',
+            fontWeight: 500,
+            lineHeight: '3.76vh',
+            textAlign: 'left',
+            color: '#FFFFFF',
+          }}>+91-0000000000</p></a>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default App
+
